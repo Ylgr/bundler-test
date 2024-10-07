3b4934454559347354a19975d5c981e5eb9bc59a
viet tu <quyen.luminous@gmail.com>
2024-10-07 18:09:27 +0700

    (+) smooth simulate

--- base-fee-test.js
+++ base-fee-test.js
@@ -1,18 +1,21 @@
 import {BaseFeeTestConfig, client, walletClient} from "./utils.js";
 
 async function main() {
-    // const simulateResult = await client.simulateContract({
-    //     ...BaseFeeTestConfig,
-    //     functionName: 'testRevert'
-    // })
-    // console.log('simulateResult: ', simulateResult)
-
-    const result = await walletClient.writeContract({
+    const baseFee = await client.getGasPrice()
+    console.log('baseFee: ', baseFee)
+    const simulateResult = await client.simulateContract({
         ...BaseFeeTestConfig,
         functionName: 'testRevert',
-        gas: 20000000,
-        gasPrice: 5000000000
+        gasPrice: baseFee,
     })
-    console.log('result: ', result)
+    console.log('simulateResult: ', simulateResult)
+
+    // const result = await walletClient.writeContract({
+    //     ...BaseFeeTestConfig,
+    //     functionName: 'testRevert',
+    //     gas: 20000000,
+    //     gasPrice: 5000000000
+    // })
+    // console.log('result: ', result)
 }
 main().catch(console.error);
